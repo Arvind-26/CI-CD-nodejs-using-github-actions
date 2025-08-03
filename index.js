e13c79c70d8805d6809a685f704e59a995a5d285
Arvind <152637477+Arvind-26@users.noreply.github.com>
2025-08-03 18:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require('express'); 
-  
-var app = express();//Respond with "hello world" for requests that hit our root "/" 
-app.get('/', function (req, res) { 
-  return res.status(200).send('changed not '); 
-});//listen to port 3000 by default 
-app.listen(process.env.PORT || 3000); 
-  
-module.exports = app; 
+const express = require('express');
+const app = express();
+
+// Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+  return res.status(200).send('changed not');
+});
+
+// Listen on port 3000 by default
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
